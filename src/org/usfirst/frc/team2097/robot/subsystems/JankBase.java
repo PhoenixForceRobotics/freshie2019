3b830939a118f8d4cc46a38abadfef9844e9a22c
Andrew <andrewbaruck@Andrews-MacBook-Pro.local>
2019-04-25 19:57:56 -0700

    FIRSTdaydoneYAAY

---
+++ src/org/usfirst/frc/team2097/robot/subsystems/JankBase.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team2097.robot.subsystems;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team2097.robot.Constants;
+import org.usfirst.frc.team2097.robot.subsystems.Utility.Motor;
+
+
+//This is where the motors are imported
+
+
+
+public static class JankBase extends subsystems{
+
+    public Motor DBTL1, DBTL2, DBTL3;
+    public Motor DBTR1, DBTR2, DBTR3;
+
+
+
+}
