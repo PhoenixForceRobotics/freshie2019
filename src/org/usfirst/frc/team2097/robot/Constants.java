3df6bfafbc4a5a551de56a0efa13965799f1c8cf
Vorng <emilykvorng@gmail.com>
2019-04-25 19:54:29 -0700

    start of elevator

--- src/org/usfirst/frc/team2097/robot/Constants.java
+++ src/org/usfirst/frc/team2097/robot/Constants.java
@@ -2,49 +2,52 @@ package org.usfirst.frc.team2097.robot;
 
 public class Constants {
 
-public static class MotorMap {
+    public static class MotorMap {
 
-public static class Drivebase{
-public static int DBTL1 = 1;
-public static int DBTL2 = 2;
-public static int DBTL3 = 3;
-//DRIVEBASE LEFT MOTORS
+            public static class Drivebase{
+            public static int DBTL1 = 1;
+            public static int DBTL2 = 2;
+            public static int DBTL3 = 3;
+            //DRIVEBASE LEFT MOTORS
 
-public static int DBTR1 = 12;
-public static int DBTR2 = 13;
-public static int DBTR3 =14;
-//DRIVEBASE RIGHT MOTORS
+            public static int DBTR1 = 12;
+            public static int DBTR2 = 13;
+            public static int DBTR3 =14;
+            //DRIVEBASE RIGHT MOTORS
 
-public static final boolean DBTL1_BW = false;
-public static final boolean DBTL2_BW = false;
-public static final boolean DBTL3_BW = false;
-//LEFT
+            public static final boolean DBTL1_BW = false;
+            public static final boolean DBTL2_BW = false;
+            public static final boolean DBTL3_BW = false;
+            //LEFT
 
-public static final boolean DBTR1_BW = true;
-public static final boolean DBTR2_BW = true;
-public static final boolean DBTR3_BW = true;
-//RIGHT
+            public static final boolean DBTR1_BW = true;
+            public static final boolean DBTR2_BW = true;
+            public static final boolean DBTR3_BW = true;
+            //RIGHT
 
+        }
+
+        public static class HDrive{
+
+            public static int HD1 = 15;
+            public static int HD2 = 16;
+            //HDRIVE MOTORS
+
+            public static final boolean HD1_BW = false;
+            public static final boolean HD2_BW = true;
+
+        }
+
+        public static class Elevator{
+            //Elevator
+            public static int ELEVATOR1 = 10;
+            public static int ELEVATOR2 = 11;
+
+            public static final boolean ELEVATOR1_REVERSE = true;
+            public static final boolean ELEVATOR2_REVERSE = true;
+
+        }   
+
+
+    }
 }
-
-public static class HDrive{
-
-public static int HD1 = 15;
-public static int HD2 = 16;
-//HDRIVE MOTORS
-
-public static final boolean HD1_BW = false;
-public static final boolean HD2_BW = true;
-
-}
-
-
-
-
-
-}
-}
-
-
-
-
