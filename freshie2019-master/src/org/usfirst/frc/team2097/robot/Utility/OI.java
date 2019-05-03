5780c2e1524778de7dc53e977eaa617c96e213db
Andrew <andrewbaruck@Andrews-MacBook-Pro.local>
2019-05-03 13:22:59 -0700

    Just adding somethings

--- freshie2019-master/src/org/usfirst/frc/team2097/robot/Utility/OI.java
+++ freshie2019-master/src/org/usfirst/frc/team2097/robot/Utility/OI.java
@@ -5,14 +5,30 @@ public class OI{
 
 
         BobXboxController driverController = new BobXboxController(0,0.11,0.11);
-        BobXboxController oporaterEvanController = new BobXboxController(1,0.11,0.11);
+        BobXboxController evnController = new BobXboxController(1,0.11,0.11);
                         //Evan is the coolest freshman
 
 
     public OI(){
 
-        
+            driverController.leftTrigger.whileheld(new runHDrive());
+            driverController.rightTrigger.whileheld(new runHDrive());
+            // Runs the HDrive, or the side to side wheels.
 
+            driverController.rightStick.whileheld(new RunJankBase());
+            driverController.rightStick.whileheld(new RunJankBase());
+            //These run the drivebase, and they are the sticks on the top of the controller
+
+            //driverController.yButton.whenpressed(new RunShifter());
+            //driverController.bButton.whenpressed(new runShifter());
+            //I am not using these anymore,,
+
+            driverController.rightStickButton.whenpressed(new runShifter());
+            //this is the one to run the shifter
+
+            driverController.leftTriggerButton.whileheld(new slowHDrive());
+            driverController.RightTreggerButton.whileheld(new slowHDrive());
+            //This is a code I plan to write to make the HDrive run slower.
 
 
 
